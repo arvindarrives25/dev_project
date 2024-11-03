ee54475dc221966a893ea8f7803c73d15fa7e03b
arvindarrives25 <arvindarrives25>
2024-11-03 13:57:31 +0000

    commit

--- Notebook1.py
+++ Notebook1.py
@@ -1,4 +1,16 @@
 # Databricks notebook source
 print("This is Notebook1 with new feature")
-dbutils.notebook.run("/Workspace/Repos/monika_kumar5@outlook.com/dev_project/Notebook2", 60)
+
+# Set environment-specific base path
+env = "dev"  # Change to "prod" in prod_project
+
+if env == "dev":
+    base_path = "/Workspace/Repos/monika_kumar5@outlook.com/dev_project"
+else:
+    base_path = "/Workspace/Repos/monika_kumar5@outlook.com/prod_project"
+
+# Use base_path to call Notebook2
+dbutils.notebook.run(f"{base_path}/Notebook2", timeout_seconds=600)
+
+print("latest changes to prod")
 
